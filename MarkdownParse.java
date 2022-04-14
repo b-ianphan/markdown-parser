cee3dfe9ad0b21fe30964b82ed8367d05946f213
b-ianphan <baphan@ucsd.edu>
2022-04-14 15:34:07 -0700

    changing my test and fixing my main code

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,8 +15,12 @@ public class MarkdownParse {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            int closeParen = markdown.indexOf(")", openParen); 
+                if( openParen == -1 || closeParen == -1 ){
+                    return toReturn;
+                } else {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
             currentIndex = closeParen + 1;
         }
 

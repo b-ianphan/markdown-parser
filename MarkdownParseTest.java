60b0c794cafa647607830456864624b41e89d0dd
b-ianphan <baphan@ucsd.edu>
2022-04-21 22:37:42 -0700

    doing lab4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,6 +9,12 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Testpublic void testGetLinks(){
+        MarkdownParse mdp = new MarkdownParse();
+        String[] expected = {"https://something.com", "some-page.html"};
+        assertArrayEquals(expected, mdp.getLinks(Files.readString(test-file.md)));
     }
 
 }
